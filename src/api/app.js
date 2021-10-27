c72c194278b74b8179534b7cf397de20f186c7f2
hheloisa <hhackenhaar@gmail.com>
2021-10-27 14:52:07 -0300

    Cria recurso /users

--- src/api/app.js
+++ src/api/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const usersRoutes = require('../routes/users');
 
 const app = express();
+
+app.use('/users', usersRoutes);
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
