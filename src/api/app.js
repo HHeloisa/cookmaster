f9c2960b9632c7273ee0b8827d7ce3d60e5f88f4
hheloisa <hhackenhaar@gmail.com>
2021-10-27 16:43:12 -0300

    Cria recurso /login

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,13 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const usersRoutes = require('../routes/users');
+const loginRoutes = require('../routes/login');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.use('/users', usersRoutes);
+app.use('/login', loginRoutes);
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
