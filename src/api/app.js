c4caf32ec35576beb741c8634232cbc509951aa3
hheloisa <hhackenhaar@gmail.com>
2021-10-27 17:23:16 -0300

    Cria recurso de /recipies

--- src/api/app.js
+++ src/api/app.js
@@ -2,12 +2,14 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const usersRoutes = require('../routes/users');
 const loginRoutes = require('../routes/login');
+const recepiesRoutes = require('../routes/recepies');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.use('/users', usersRoutes);
 app.use('/login', loginRoutes);
+app.use('/recipes', recepiesRoutes);
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
