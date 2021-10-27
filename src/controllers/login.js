9b4f2ca29f61c0fb661f8839f8cc981db055c340
hheloisa <hhackenhaar@gmail.com>
2021-10-27 17:41:59 -0300

    Atualiza rota de recepies inserindo middlewares

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,11 +1,11 @@
+const rescue = require('express-rescue');
 const userModel = require('../models/users');
 const { status, loginMessages } = require('../messages');
 const { generateToken } = require('../middlewares/authorizations');
 
-async function userLogin(req, res) {
+const userLogin = rescue(async (req, res) => {
   const { email, password } = req.body;
   const userDB = await userModel.findByEmail(email);
-  console.log(userDB);
   if (!userDB || userDB.password !== password) {
     return res
     .status(status.unauth)
@@ -15,6 +15,6 @@ async function userLogin(req, res) {
   
   const token = generateToken(_id, email, role);
   return res.status(status.sucess).json({ token });
-}
+});
 
 module.exports = { userLogin };
--- src/routes/recepies.js
+++ src/routes/recepies.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const recepiesController = require('../controllers/recepies');
+const { verifyToken } = require('../middlewares/authorizations');
+const { validRecepies } = require('../middlewares/validateRecepies');
 
-router.post('/', recepiesController.create);
+router.post('/', verifyToken, validRecepies, recepiesController.create);
 
 module.exports = router;
