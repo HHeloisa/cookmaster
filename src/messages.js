d65c344c35092f09dd2a5c5395164e0ad494bb9a
hheloisa <hhackenhaar@gmail.com>
2021-10-27 15:04:26 -0300

    Cria arquivo auxiliar de mensagens e codigos

---
+++ src/messages.js
@@ -0,0 +1,12 @@
+const status = {
+  sucess: 200,
+  create: 201,
+  badRequest: 400,
+  unauth: 401,
+  forbidden: 403,
+  notFound: 404,
+  unprocessableEntity: 422,
+  intServerError: 500,
+};
+
+module.exports = { status };
