bb4aca104b4bfdfa942b8a5a1d15d86274691c16
hheloisa <hhackenhaar@gmail.com>
2021-11-09 18:06:18 -0300

    Cria teste para verificar se retorna um erro, ao tentar criar um usuario que ja existe

--- src/integration-tests/userLogin.test.js
+++ src/integration-tests/userLogin.test.js
@@ -12,6 +12,12 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
+const userMock = {
+  name: 'Heloísa J. Hackenahar',
+  email: 'hhackenhaar@gmail.com',
+  password: '444648'
+}
+
 describe('Valida a criação de um usuário em post /users', () => {
 
   describe('Cadastro de usuário realizado com sucesso', () => {
@@ -71,6 +77,8 @@ describe('Valida a criação de um usuário em post /users', () => {
       sinon.stub(MongoClient, 'connect')
       .resolves(connectionMock);
 
+      // cria um usuario agora, para testar depois o que retorna ao criar um usuario que já existe.
+      connectionMock.db('Cookmater').collection('users').insertOne(userMock);
     });
 
     after(async () => {
@@ -104,6 +112,15 @@ describe('Valida a criação de um usuário em post /users', () => {
       expect(response.body).to.have.property('message');
       expect(response.body.message).to.be.equal('Invalid entries. Try again.');
     });
+    it('Retorna erro ao tentar criar um usuário que já existe', async () => {
+      response = await chai.request(server)
+        .post('/users')
+        .send({ name: 'Heloísa J Hackenhaar', email: 'hhackenhaar@gmail.com', password: '444648' });
+
+      expect(response).to.have.status(409);
+      expect(response.body).to.have.property('message');
+      expect(response.body.message).to.be.equal('Email already registered');
+    })
   }); 
   
 });
