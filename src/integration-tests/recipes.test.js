4f33010b5d40022d967c3a81ba6472262db0891e
hheloisa <hhackenhaar@gmail.com>
2021-11-09 20:15:04 -0300

    Cria teste que verifica que uma receita criada possui name, ingredientes, preparation, userId, e _id

--- src/integration-tests/recipes.test.js
+++ src/integration-tests/recipes.test.js
@@ -17,8 +17,13 @@ const userMock = {
   password: '444648'
 }
 
-describe.only('Testes da rota GET /recipes', () => {
-  
+const recipeMock = {
+  name: 'Lasanha vegana',
+  ingredients: 'Panequeca, Brocolis, Alho, FakeCheddar',
+  preparation: '2 horas'
+}
+
+describe('Testes da rota GET /recipes', () => { 
   describe('testa caso de sucesso em buscar todas as receitas', () => {
     let response;
 
@@ -51,5 +56,48 @@ describe.only('Testes da rota GET /recipes', () => {
       expect(response.body).to.be.an('array');
     });
   });
-  describe('testa caso de erro na publicação da receita', () => {});
 })
+
+describe.only('Testes da rota POST /recipes', () => {
+  describe('Testa caso de sucesso em publicar uma receita', () => {
+    let response;
+
+    before(async () => {
+      const connectionMock = await getMockConnection();
+      sinon.stub(MongoClient, 'connect')
+      .resolves(connectionMock);
+
+      const usersCollection = connectionMock.db('Cookmaster').collection('users');
+      await usersCollection.insertOne(userMock);
+  
+      const token = await chai.request(server)
+        .post('/login')
+        .send({
+          email: 'hhackenhaar@gmail.com',
+          password: '444648'
+        })
+        .then((res) => res.body.token);
+  
+      response = await chai.request(server)
+        .post('/recipes')
+        .set('Authorization', token)
+        .send({
+          name: 'Lasanha vegana',
+          ingredients: 'Panequeca, Brocolis, Alho, FakeCheddar',
+          preparation: '2 horas'
+        });
+      });
+    it('adiciona uma receita, retorna status 201, e objeto "recipe"', async() => {
+      expect(response).to.have.status(201);
+      expect(response.body).to.be.an('object')
+      expect(response.body).to.be.property('recipe');
+    })
+    it('verifica se objeto "recipe" contém: name, ingredients, preparation, userId e _id ', async() => {
+      expect(response.body.recipe).to.be.property('name');
+      expect(response.body.recipe).to.be.property('ingredients')
+      expect(response.body.recipe).to.be.property('preparation')
+      expect(response.body.recipe).to.be.property('userId')
+      expect(response.body.recipe).to.be.property('_id')
+    })
+  })
+ });
