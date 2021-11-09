1c450f89fbd4cee7091483b9be975c321eaa662c
hheloisa <hhackenhaar@gmail.com>
2021-11-09 17:38:33 -0300

    Remove teste de password no body de response, e cria testes de role e _id

--- src/integration-tests/userLogin.test.js
+++ src/integration-tests/userLogin.test.js
@@ -34,15 +34,29 @@ describe('Valida a criação de um usuário em post /users', () => {
         });
     });
     it('Espera que o retorno em body seja um objeto com a propriedade "user"', () => {
-      console.log('to aqui dentr do teste');
       expect(response.body).to.be.an('object')
       expect(response.body).to.be.property('user');
     });
-    it('Esperar que o retorno a propriedade "user" tenha a propriedade "name"', () => {
+    it('Espera que o objeto "user" tenha a propriedade "name"', () => {
       expect(response.body.user).to.be.property('name');
+      expect(response.body.user['name']).to.be.a('string');
       expect(response.body.user.name).to.be.equal('Heloísa J. Hackenhaar');
-    })
+    });
+    it('Espera que o objeto "user" tenha a propriedade "email"', () => {
+      expect(response.body.user).to.be.property('email');
+      expect(response.body.user['email']).to.be.a('string');
+      expect(response.body.user.email).to.be.equal('hhackenhaar@gmail.com');
+    });
+    it('Espera que o objeto "user" tenha a propriedade "_id"', () => {
+      expect(response.body.user).to.be.property('_id');
+      expect(response.body.user['_id']).to.be.a('string');
+    });
+    it('Espera que o objeto "user" tenha a propriedade "role"', () => {
+     
+      expect(response.body.user).to.be.property('role');
+      expect(response.body.user['role']).to.be.a('string');
+      expect(response.body.user.role).to.be.equal('user');
+    });
   });
-
 }); 
 
