d2e90e5510a7b6c58a676da867f15cbd185ec890
hheloisa <hhackenhaar@gmail.com>
2021-11-22 18:50:26 -0300

    Adiciona verificaçao ao middleware, para evitar que quebre

--- src/middlewares/validateAdminOrOwner.js
+++ src/middlewares/validateAdminOrOwner.js
@@ -1,11 +1,14 @@
-const { status, usersMessages } = require('../messages');
+const { status, usersMessages, recipesMessages } = require('../messages');
 const { getRecipeById } = require('../models/recipes');
 
 const verifyAdminOrOwner = async (req, res, next) => {
   const { id } = req.params;
   const { role, _id } = req.user;
-  const recipe = await getRecipeById(id);
-  if (role !== 'admin' && JSON.stringify(_id) !== JSON.stringify(recipe.userId)) {
+  const recipeById = await getRecipeById(id);
+  if (!recipeById) {
+    return res.status(status.notFound).json({ message: recipesMessages.notFound });
+  }
+  if (role !== 'admin' && JSON.stringify(_id) !== JSON.stringify(recipeById.userId)) {
     return res.status(status.forbidden).json({ message: usersMessages.onlyAdmin });
   }
   next();
